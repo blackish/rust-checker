9c050ec334fa6c103144fd192ba82ebeb7fab44d
blackish <andrey.lysov@protonmail.com>
2023-06-19 07:31:27 +0100

    graphite not tested

--- src/output_graphite.rs
+++ src/output_graphite.rs
@@ -4,33 +4,63 @@ use crate::config::OutputConfig;
 use std::net::TcpStream;
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::time::{Duration, Instant};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::thread::sleep;
+
+const MIN_RECONNECT_DELAY_MS: u64 = 50;
+const MAX_RECONNECT_DELAY_MS: u64 = 10_000;
 
 pub struct GraphiteOutput {
+    prefix: String,
+    name: String,
+    buffer: Vec<String>,
     socket: RetrySocket
 }
 
 struct RetrySocket {
     socket: Option(TcpStream),
-    buf: Vec<CheckResult>
+    address: Vec<SocketAddr>,
+    next_try: Instant,
+    retries: usize
 }
 
 impl RetrySocket {
     fn new<A: ToSocketAddrs>(addresses: A) -> io::Result<Self> {
         let sockaddrs = addresses.to_socket_addrs()?.collect();
-        sock = Self {address: sockaddrs, socket: None};
+        sock = Self {
+            address: sockaddrs,
+            retries: 0,
+            next_try: Instant::now(),
+            socket: None
+        };
+        let _ = sock.flush().ok();
         Ok(sock)
     }
     fn try_connect(&mut self) -> io::Result<()> {
         if self.socket.is_none() {
             let now = Instant::now();
-            if now > self.next_try {
-                let addresses: &[SocketAddr] = self.addresses.as_ref();
-                let socket = TcpStream::connect(addresses)?;
-                self.socket = Some(socket);
+            if now < self.next_try {
+                sleep(self.next_try - now);
+            }
+            let addresses: &[SocketAddr] = self.addresses.as_ref();
+            let conn = TcpStream::connect(addresses);
+            match conn {
+                Ok(socket) => {
+                    self.socket = socket;
+                    Ok(())
+                }
+                Err(e) => {
+                    self.retries += 1;
+                    let exp_delay = MIN_RECONNECT_DELAY_MS << self.retries;
+                    let max_delay = MAX_RECONNECT_DELAY_MS.min(exp_delay);
+                    self.next_try = now + Duration::from_millis(max_delay);
+                    Err(e)
+                }
             }
         }
         Ok(())
     }
+
     fn with_socket<F, T>(&mut self, operation: F) -> io::Result<T>
     where
         F: FnOnce(&mut TcpStream) -> io::Result<T>,
@@ -66,12 +96,30 @@ impl Write for RetrySocket {
 
 impl GraphiteOutput {
     pub fn new(config: &OutputConfig) -> Self {
-        return Self{socket: RetrySocket::new(config.config.get("address").unwrap().to_string()).unwrap(), buf: Vec::new()}
+        Self{
+            socket: RetrySocket::new(config.config.get("address").unwrap().to_string()).unwrap(),
+            buffer: Vec::new(),
+            prefix: config.config.get("prefix").unwrap().to_string()
+        }
     }
 }
 
 impl Outputs for GraphiteOutput {
     fn process_probe(&mut self, probe: CheckResult) {
-        self.buf.push(probe.clone());
+        for (key, value) in probe.values {
+            let mut received = self.prefix.clone();
+            if self.name != "" && let Some(name) == probe.labels.get(self.name) {
+                received += name + "_"
+            }
+            received += format!("{} {} {}\n", key, value, SystemTime.now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string());
+            self.buf.push(received);
+        }
+        let to_send = self.buf.drain().collect();
+        for probe in to_send {
+            match self.sock.write_all(probe.as_byte()) {
+                Ok(()) => {},
+                Err(_) => {self.buf.push(probe);}
+            }
+        }
     }
 }
