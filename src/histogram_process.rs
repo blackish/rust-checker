e554b7eec3cb33a7266f520bd9a52d967f994521
blackish <andrey.lysov@protonmail.com>
2023-06-29 07:46:05 +0100

    histogram

--- src/histogram_process.rs
+++ src/histogram_process.rs
@@ -10,8 +10,8 @@ use std::time::Duration;
 
 pub struct Histogram {
     keep_name: bool,
-    stats_to_emit: Vec<f64>,
-    stats: Arc<Mutex<HashMap<String, Vec<EmitStats>>>>,
+    //stats_to_emit: Vec<f64>,
+    stats: Arc<Mutex<HashMap<String, Vec<f32>>>>,
     probes: Arc<Mutex<HashMap<String, CheckResult>>>,
     to_process: String,
     interval: i32,
@@ -20,15 +20,14 @@ pub struct Histogram {
     id: u16
 }
 
-fn emit_probes(interval: i32, stats: Arc<Mutex<HashMap<String, Vec<EmitStats>>>>, probes: Arc<Mutex<HashMap<String, CheckResult>>>, sender: Sender<CheckResult>) {
+fn emit_probes(interval: i32, stats: Arc<Mutex<HashMap<String, Vec<f32>>>>, probes: Arc<Mutex<HashMap<String, CheckResult>>>, sender: Sender<CheckResult>) {
     let duration = Duration::from_secs(interval.try_into().unwrap());
-    println!("{:?}", duration);
     loop {
         sleep(duration);
         let mut to_emit_stats = stats.lock().unwrap();
         let to_emit_probes = probes.lock().unwrap();
+        let mut n_probe: i32 = 0;
         for (key, mut value) in to_emit_stats.drain() {
-            let mut n_probe: i32 = 1;
             let probe_to_copy = to_emit_probes.get(&key).unwrap().clone();
             let mut new_probe = CheckResult {
                 name: probe_to_copy.name.clone(),
@@ -36,17 +35,9 @@ fn emit_probes(interval: i32, stats: Arc<Mutex<HashMap<String, Vec<EmitStats>>>>
                 processes: probe_to_copy.processes.clone(),
                 values: HashMap::new()
             };
-            if let EmitStats::Number(n) = value.pop().unwrap() {
-                n_probe = n;
-            }
             for atomic_stat in value {
-                match atomic_stat {
-                    EmitStats::Sum(s) => _ = new_probe.values.insert(String::from("sum"), s),
-                    EmitStats::Avg(s) => _ = new_probe.values.insert(String::from("avg"), s / n_probe as f32),
-                    EmitStats::Low(s) => _ = new_probe.values.insert(String::from("low"), s),
-                    EmitStats::High(s) => _ = new_probe.values.insert(String::from("high"), s),
-                    EmitStats::Number(_) => {}
-                }
+                new_probe.values.insert(format!("{}", n_probe), atomic_stat);
+                n_probe += 1;
             }
             sender.send(new_probe).unwrap();
         }
@@ -57,7 +48,7 @@ impl Histogram {
     pub fn new(config: &ProcessConfig, sender: Sender<CheckResult>, receiver: Receiver<CheckResult>) -> Self {
         let result = Self {
             keep_name: config.keep_name.clone(),
-            stats_to_emit: Vec::new(),
+            //stats_to_emit: Vec::new(),
             stats: Arc::new(Mutex::new(HashMap::new())),
             probes: Arc::new(Mutex::new(HashMap::new())),
             to_process: config.match_value.clone(),
@@ -66,14 +57,14 @@ impl Histogram {
             labels_to_add: config.labels_to_add.clone(),
             receiver: receiver
         };
-        match config.config.get("values") {
-            yaml_rust::Yaml::Array(ref h) => {
-                for v in h {
-                    result.stats_to_emit.push(v.clone().into_f64().unwrap());
-                }
-            },
-            _ => {}
-        }
+        //match config.config.get("values") {
+            //yaml_rust::Yaml::Array(ref h) => {
+                //for v in h {
+                    //result.stats_to_emit.push(v.clone().into_f64().unwrap());
+                //}
+            //},
+            //_ => {}
+        //}
         let stats = Arc::clone(&result.stats);
         let probes = Arc::clone(&result.probes);
         let interval = result.interval.clone();
@@ -104,40 +95,10 @@ impl Processes for Histogram {
                     }
                     new_probe_to_emit.insert(probe.name.clone(), new_probe);
             }
-            let mut low: f64 = -99999;
-            new_result.push(0.0);
-            for s in &self.stats_to_emit {
-                new_result.push(0.0);
-            }
-            new_result.push(0.0);
-
             let to_process = probe.values.get(&self.to_process).unwrap().clone();
             let mut new_stats = self.stats.lock().unwrap();
             new_stats.entry(probe.name.clone()).and_modify(|e| {
-                for atomic_stat in e {
-                    match atomic_stat {
-                        EmitStats::Sum(s) => {
-                            *s += to_process;
-                        },
-                        EmitStats::Avg(s) => {
-                            *s += to_process;
-                        },
-                        EmitStats::Low(s) => {
-                            if *s > to_process {
-                                *s = to_process.clone();
-                            }
-                        },
-                        EmitStats::High(s) => {
-                            if *s < to_process {
-                                *s = to_process.clone();
-                            }
-                        },
-                        EmitStats::Number(s) => {
-                            *s += 1;
-                            processed_probes = s.clone();
-                        }
-                    }
-                }
+                e.push(to_process);
             }).or_insert(new_result);
         }
     }
